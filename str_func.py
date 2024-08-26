28f904aef708ca26f899852b9ec3a6d641a02b4c
amba404 <amba.avg@gmail.com>
2024-08-26 16:58:18 +0900

    Revert "Feature/task3"

--- str_func.py
+++
@@ -1,9 +0,0 @@
-def get_upper(s: str):
-    """get UPPER string"""
-    return s.upper()
-
-
-def get_capitalise(s: str):
-    """ get all words in string capitalized"""
-    l = [i.capitalize() for i in s.split()]
-    return ' '.join(l)
